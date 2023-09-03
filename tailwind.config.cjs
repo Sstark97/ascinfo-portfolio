28ef63dbb8016d83de6f0c3a73d13cb2d90901dd
Sstark97 <aitor@leanmind.es>
2023-09-03 20:48:02 +0100

    :sparkles: Add new css variables and canonical url to layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,22 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: "#1d1d1d",
+				color: "#fff",
+				primary: "#00DFFC",
+				hover_btn: "#00dffcbf",
+				secondary: "#909096",
+				tertiary: "#181818",
+				fourth: "#2a2a2a",
+				cursor: "rgba(255, 255, 255, 0.8)"
+			},
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			}
+		},
 	},
 	plugins: [],
 });
