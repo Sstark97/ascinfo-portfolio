a74948162540c75a780676a5cf79473811e90f76
Sstark97 <aitor@leanmind.es>
2023-09-14 20:45:42 +0100

    Test(:white_check_mark:): getPost should return a single post

--- src/lib/application/post/getPost/getPost.action.test.ts
+++ src/lib/application/post/getPost/getPost.action.test.ts
@@ -1,6 +1,14 @@
 import type {Post} from "../../../domain/model/Post.ts";
 import type {PostRepository} from "../repository/postRepository";
 
+class GetPost {
+    constructor(private readonly postRepository: PostRepository) {}
+
+    async execute(slug: string) {
+        return await this.postRepository.findPostBySlug(slug);
+    }
+}
+
 describe("getPost should", () => {
     it("return a single post", async () => {
         const post: Post = {
@@ -13,11 +21,11 @@ describe("getPost should", () => {
 
         const PostRepository: PostRepository = {
             findAllPosts: vi.fn(),
-            findPostBySlug: vi.fn()..mockResolvedValue(post),
+            findPostBySlug: vi.fn().mockResolvedValue(post),
         }
         const getPost = new GetPost(PostRepository);
 
-        const singlePost = await getPost.execute(post.slug);
+        const singlePost = await getPost.execute("slug");
 
         expect(singlePost).toEqual(post);
     });
