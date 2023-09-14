c3693b0d7d4feb55187075da5e7db6da54d852f3
Sstark97 <aitor@leanmind.es>
2023-09-14 20:03:04 +0100

    :recycle: create zod schema with Post interface

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
 import { defineCollection } from 'astro:content';
-import {z} from "astro/zod";
+import {z, ZodSchema} from "astro/zod";
+import type {Post} from "../lib/domain/model/Post";
 
+const postSchema: ZodSchema<Post> = z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
+    canonical_url: z.string().optional(),
+    date: z.string(),
+    tags: z.array(z.string()),
+});
+
+
+// create zod collection with Post interface
 const postCollection = defineCollection({
     type: 'content', // v2.5.0 y posteriores
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        image: z.string().optional(),
-        canonical_url: z.string().optional(),
-        date: z.string(),
-        tags: z.array(z.string()),
-    }),
+    schema: postSchema,
 });
 
 export const collections = {
---
+++ src/lib/domain/model/Post.ts
@@ -0,0 +1,8 @@
+export interface Post {
+    title: string;
+    description: string;
+    image?: string;
+    canonical_url?: string;
+    date: string;
+    tags: string[];
+}
