1e62403b54e2ab12208260c84e70ad763fb7408a
Sstark97 <aitor@leanmind.es>
2023-09-27 21:52:04 +0100

    :bug: Fix deploy with add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp'],
+    },
+  },
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -13,8 +20,10 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ["javascript", "typescript", "jsx", "tsx"],
+      langs: ["javascript", "typescript", "jsx", "tsx"]
     }
   },
   integrations: [tailwind(), react(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
