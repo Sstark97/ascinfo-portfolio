a3fca68a79735d89c030ff02f2d37683a80c73e3
Sstark97 <aitor@leanmind.es>
2023-10-01 21:58:15 +0100

    Test(:rotating_light:): CodeBlockToCopy should show a clipboard check icon when copy the content

--- src/components/react/codeBlockToCopy/CodeBlockToCopy.test.tsx
+++ src/components/react/codeBlockToCopy/CodeBlockToCopy.test.tsx
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import {CodeBlockToCopy} from "@components/react/codeBlockToCopy/CodeBlockToCopy.tsx";
 import userEvent from "@testing-library/user-event";
+import {expect} from "vitest";
 
 describe("CodeBlockToCopy", () => {
     it("should render", () => {
@@ -17,5 +18,14 @@ describe("CodeBlockToCopy", () => {
         await userEvent.click(copyButton);
 
         expect(onCopy).toHaveBeenCalledWith(toCopy);
+    });
+    it('should show a clipboard check icon when copy the content', async () => {
+        const onCopy = vi.fn();
+        render(<CodeBlockToCopy onCopy={onCopy}><p></p></CodeBlockToCopy>)
+
+        const copyButton = screen.getByLabelText("Copy code block");
+        await userEvent.click(copyButton);
+
+        expect(screen.getByRole("graphics-document", { name: "copy-checked" })).toBeInTheDocument();
     });
 });
