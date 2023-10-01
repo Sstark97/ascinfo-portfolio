73904293618f6c99bd4abcc6313f6ab933dd4940
Sstark97 <aitor@leanmind.es>
2023-10-01 16:53:15 +0100

    Test(:recycle:): GetAllProjects should return projects sorted by date in ascending order -> rename sort params and extract to another function

--- src/lib/application/project/getAllProjects/getAllProjects.action.ts
+++ src/lib/application/project/getAllProjects/getAllProjects.action.ts
@@ -1,10 +1,17 @@
 import type {ProjectRepository} from "../repository/ProjectRepository.ts";
+import type {Project} from "../../../domain/model/Project.ts";
 
 export class GetAllProjects {
     constructor(private readonly projectRepository: ProjectRepository) {}
     async execute() {
         const allProjects = await this.projectRepository.findAllProjects();
 
-        return allProjects.sort((a, b) => b.date.getTime() - a.date.getTime());
+        return this.getProjectsOrderByDateAsc(allProjects);
+    }
+
+    private getProjectsOrderByDateAsc(allProjects: Project[]) {
+        return allProjects.sort((currentProject, nextProject) => (
+            nextProject.date.getTime() - currentProject.date.getTime())
+        );
     }
 }
