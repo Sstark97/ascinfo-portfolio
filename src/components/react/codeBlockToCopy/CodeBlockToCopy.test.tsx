3641fa11ae8a6c5376d5bbc12da528fd97839061
Sstark97 <aitor@leanmind.es>
2023-10-01 21:49:32 +0100

    Test(:rotating_light:): CodeBlockToCopy should copy the content of children

--- src/components/react/codeBlockToCopy/CodeBlockToCopy.test.tsx
+++ src/components/react/codeBlockToCopy/CodeBlockToCopy.test.tsx
@@ -1,10 +1,21 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {CodeBlockToCopy} from "@components/react/codeBlockToCopy/CodeBlockToCopy.tsx";
+import userEvent from "@testing-library/user-event";
 
 describe("CodeBlockToCopy", () => {
     it("should render", () => {
-        const { container } = render(<CodeBlockToCopy><p></p></CodeBlockToCopy>)
+        const {container} = render(<CodeBlockToCopy><p></p></CodeBlockToCopy>)
 
         expect(container).toBeInTheDocument();
+    });
+    it('should copy the content of children', async () => {
+        const toCopy = "content to copy"
+        const onCopy = vi.fn();
+        render(<CodeBlockToCopy onCopy={onCopy}><p>{toCopy}</p></CodeBlockToCopy>)
+
+        const copyButton = screen.getByLabelText("Copy code block");
+        await userEvent.click(copyButton);
+
+        expect(onCopy).toHaveBeenCalledWith(toCopy);
     });
 });
